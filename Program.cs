aab8a722821b842a4d7956da765f3f58277db403
Gabriel Lindsay <gabelindsay9@gmail.com>
2023-10-03 10:13:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_4_Random_Numbers_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
