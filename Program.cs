25f5587bc17e0acd44b2989dfe8ad6c91d0d7a81
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-11-08 09:55:15 -0500

    done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Part_4_Random_Numbers_Assignment
+﻿using System.Runtime.CompilerServices;
+
+namespace Part_4_Random_Numbers_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random generator = new Random();
+            int minNum;
+            int maxNum;
+            Console.WriteLine("Please enter a minimum and maximum value below");
+            Console.WriteLine("Enter a minimum value:");
+            minNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a maximum value:");
+            maxNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.WriteLine();
+            Console.WriteLine("Now enter in two decimal values using the minimum and maximum values");
+            minNum = Convert.ToInt32(Console.ReadLine());
+            maxNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.Write(generator.Next(minNum, maxNum) + " ");
+            Console.Write(generator.Next(minNum, maxNum) + " ");
         }
     }
 }
